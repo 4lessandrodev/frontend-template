1d9d2b7f233b11cbdb83cc02a21c84977cad1703
4lessandrodev <alessandroadm@live.com>
2022-06-28 23:14:59 -0300

    feat(login): added login logic

--- domain/login.js
+++ domain/login.js
@@ -1,33 +1,101 @@
 const form = document.querySelector('.form');
+const errorMessage = form.querySelector('.error');
+const successMessage = form.querySelector('.success');
 
 const database = {
-	user: { nick: 'admin', password: '123456' }
+	user: {
+		nick: 'admin',
+		password: '123456'
+	}
 };
 
-
-if (form) form.addEventListener('submit', function (event) {
-	const { user } = database;
-	event.preventDefault();
+function getNickAndPassword() {
 	const passwordInput = form.querySelector("input[type=password]");
 	const nickInput = form.querySelector("input[type=text]");
 
 	const nick = nickInput.value;
 	const password = passwordInput.value;
-	const errorMessage = form.querySelector('.error');
-	const successMessage = form.querySelector('.success');
 
-	if ((nick !== user.nick || password !== user.password) && form) {
-		if (errorMessage) {
-			if (successMessage) successMessage.classList.add('hidden');
-			errorMessage.classList.remove('hidden');
-			errorMessage.firstChild.innerText = 'Error: invalid credentials.';
-		}
-	} else {
-		if (successMessage) {
-			successMessage.classList.remove('hidden');
-			successMessage.firstChild.innerText = 'Success: connected';
-			if (errorMessage) errorMessage.classList.add('hidden');
-			form.reset();
-		}
-	}
+	return {
+		nick,
+		password
+	};
+}
+
+function login() {
+	const {
+		user
+	} = database;
+	const {
+		nick,
+		password
+	} = getNickAndPassword();
+	return (nick !== user.nick || password !== user.password) ? showErrorMessage() : showSuccessMessage();
+}
+
+function showSuccessMessage(message = 'Success: connected') {
+	successMessage && successMessage.classList.remove('hidden');
+	successMessage.firstChild.innerText = message;
+	return true;
+}
+
+function hideSuccessMessage() {
+	successMessage && successMessage.classList.add('hidden');
+}
+
+function hideErrorMessage() {
+	errorMessage && errorMessage.classList.add('hidden');
+}
+
+function showErrorMessage(message = 'Error: invalid credentials.') {
+	errorMessage && errorMessage.classList.remove('hidden');
+	errorMessage ? errorMessage.firstChild.innerText = message : false;
+	return false;
+}
+
+function showLoading(event) {
+	const loading = event.target.querySelector(".loading");
+	loading && loading.classList.remove('hidden');
+}
+
+function hidesLoading(event) {
+	const loading = event.target.querySelector(".loading");
+	loading && loading.classList.add('hidden');
+}
+
+function getCookieExpiration() {
+	const currentDate = new Date();
+	currentDate.setHours(currentDate.getHours() + 1);
+	return currentDate.toGMTString();
+}
+
+function handleSuccess() {
+	const expiration = getCookieExpiration();
+	document.cookie = `user=authenticated;Expires=${expiration}`;
+	redirectToDashboard()
+}
+
+function redirectToDashboard() {
+	location.href = '#dashboard';
+}
+
+function redirectToLogin() {
+	location.href = '#';
+}
+
+form && form.addEventListener('submit', function (event) {
+	event.preventDefault();
+	hideErrorMessage();
+	hideSuccessMessage();
+	showLoading(event);
+	setTimeout(() => {
+		const isSuccess = login(event.target);
+		(isSuccess) ? handleSuccess(): redirectToLogin();
+	}, 1000);
+	setTimeout(() => hidesLoading(event), 1000);
 });
+
+(function checkAuth() {
+	const isAuthenticated = document.cookie.includes('user');
+	(isAuthenticated) ? redirectToDashboard() : redirectToLogin();
+})();

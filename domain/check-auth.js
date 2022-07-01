2d9516365c4bb6c71c9e64201de995c4ffcf3c8a
4lessandrodev <alessandroadm@live.com>
2022-07-01 11:35:44 -0300

    fix(route): change initial route

--- domain/check-auth.js
+++ domain/check-auth.js
@@ -1,9 +1,28 @@
+const isGithub = location.hostname === '4lessandrodev.github.io';
+const pathName = location.pathname;
+const githubPrefix = '/frontend-template';
+
+const routes = {
+	home: '/home.html',
+	login: '/index.html'
+}
+
 function redirectToDashboard() {
-	if(location.pathname !== '/home.html') location.href = '/home.html';
+	if (isGithub) {
+		if (pathName !== githubPrefix + routes.home)
+			location.href = githubPrefix + routes.home;
+		return;
+	} 
+	if(pathName !== routes.home) location.href = routes.home;
 }
 
 function redirectToLogin() {
-	if(location.pathname !== '/#') location.href = '/#';
+	if (isGithub) {
+		if (pathName !== githubPrefix + routes.login)
+			location.href = githubPrefix + routes.login;
+		return;
+	}
+	if(pathName !== routes.login) location.href = routes.login;
 }
 
 (function checkAuth() {
--- domain/login.js
+++ domain/login.js
@@ -1,6 +1,9 @@
 const form = document.querySelector('.form');
 const errorMessage = form.querySelector('.error');
 const successMessage = form.querySelector('.success');
+const isGithub = location.hostname === '4lessandrodev.github.io';
+const pathName = location.pathname;
+const githubPrefix = '/frontend-template';
 
 const database = {
 	user: {
@@ -9,18 +12,34 @@ const database = {
 	}
 };
 
+const routes = {
+	home: '/home.html',
+	login: '/index.html'
+}
+
 function redirectToDashboard() {
-	if(location.pathname !== '/home.html') location.href = '/home.html';
+	if (isGithub) {
+		if (pathName !== githubPrefix + routes.home)
+			location.href = githubPrefix + routes.home;
+		return;
+	} 
+	if(pathName !== routes.home) location.href = routes.home;
 }
 
 function redirectToLogin() {
-	if(location.pathname !== '/#') location.href = '/#';
+	if (isGithub) {
+		if (pathName !== githubPrefix + routes.login)
+			location.href = githubPrefix + routes.login;
+		return;
+	}
+	if(pathName !== routes.login) location.href = routes.login;
 }
 
 (function checkAuth() {
 	const isAuthenticated = document.cookie.includes('user');
-	(isAuthenticated) && redirectToDashboard();
+	(isAuthenticated) ? redirectToDashboard() : redirectToLogin();
 })();
+
 
 function getNickAndPassword() {
 	const passwordInput = form.querySelector("input[type=password]");
